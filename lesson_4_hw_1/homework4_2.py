1636b802e0d1ab1397f852f4eb514ed11cba3365
Kosyan <kola.narisovan@gmail.com>
2023-05-07 02:20:32 +0300

    homework

--- lesson_4_hw_1/homework4_2.py
+++ lesson_4_hw_1/homework4_2.py
@@ -2,17 +2,15 @@ while True:
     a = input("Введіть список значень розділених знаком ',' без пробілів: ")
     a = a.split(',')
 
-    if len(a) == 0:
-        print("[[], []]")
-    else:
-        middle = len(a) // 2
-        if len(a) % 2 != 0:
-            middle += 1
+    middle = len(a) // 2
 
-        first_half = a[:middle]
-        second_half = a[middle:]
+    if len(a) % 2 != 0:
+        middle += 1
 
-        if first_half == ['']:
-            first_half = []
-        b = [first_half, second_half]
-        print(b)
+    first_half = a[:middle]
+    second_half = a[middle:]
+
+    if first_half == ['']:
+        first_half = []
+    b = [first_half, second_half]
+    print(b)
